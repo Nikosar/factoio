39548e173b46a682e022cd323123303a25dca831
Nikosar <randomregistr@gmail.com>
2024-04-10 08:54:10 +0200

    add deathworld settings

--- init.sh
+++ init.sh
@@ -2,14 +2,19 @@
 mkdir -p /opt/factorio/config
 cp -r ./config /opt/factorio
 chown 845:845 /opt/factorio
+echo "Enter factorio username: "
+read factorio_username
+echo "Enter user token (https://factorio.com/profile): "
+read factorio_token
+
 sudo docker run -d \
   -p 34197:34197/udp \
   -p 27015:27015/tcp \
   -v /opt/factorio:/factorio \
-  -e TOKEN=\
-  -e USERNAME=\
+  -e TOKEN=$factorio_token\
+  -e USERNAME=$factorio_username\
   --name factorio \
   --restart=unless-stopped \
   factoriotools/factorio:stable
-#https://factorio.com/profile
+
 
